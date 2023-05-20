a40b2dbf75945549a9a2e1b97d58ec1d9b18de30
Thomas Casali <casali.thomas@gmail.com>
2023-05-20 10:17:14 +0200

    Update bot.js
    token

--- botTelegram/bot.js
+++ botTelegram/bot.js
@@ -3,6 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = 'TELEGRAM-TOKEN';
 
+
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
