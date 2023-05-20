6d9b52999beb533164c7ef5460c1e1d3f04f7e82
Thomas Casali <casali.thomas@gmail.com>
2023-05-20 08:30:55 +0200

    aggiornamento generale

---
+++ PallinaWF/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using Newtonsoft.Json;
+
+namespace PallinaWF
+{
+    public partial class Form1 : Form
+    {
+        static string URL = "wss://pallina-p5j-telegram.glitch.me";
+        WebSocket wsClient = new WebSocket(URL);
+        Posizione posCorrente = new Posizione();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {      
+            wsClient.OnMessage += WsClient_OnMessage;
+            wsClient.Connect();
+            //wsClient.Send("Ciao da C#");
+            
+        }
+
+        private void WsClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            //MessageBox.Show(e.Data);
+            try
+            {
+                posCorrente = JsonConvert.DeserializeObject<Posizione>(e.Data);
+                //MessageBox.Show(pos.x.ToString());
+                
+                pe.Invalidate();
+                Graphics G = pe.CreateGraphics();
+                SolidBrush tratto = new SolidBrush(Color.Green);
+                G.FillEllipse(tratto, posCorrente.x, posCorrente.y, 10,10);
+                G.Dispose();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void pe_Click(object sender, EventArgs e)
+        {
+            //calcola la posizione x,y del mouse
+            Point point = pe.PointToClient(Cursor.Position);
+            //mette la posizione in un oggetto Posizione
+            Posizione posToSend = new Posizione(point.X, point.Y);
+            //serializza l'oggetto
+            string xy = JsonConvert.SerializeObject(posToSend);
+            //lo invia al server
+            wsClient.Send(xy);
+        }
+    }
+}
---
+++ botTelegram/bot.js
@@ -0,0 +1,42 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '5090881372:AAHy1xX8azdM3ohKhJywgsFJ9S5FXcCd8-w';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/coord (?:0*(?:\.\d+)?|1(\.0*)?)([ ,])(?:0*(?:\.\d+)?|1(\.0*)?)/, function(msg, match) {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    console.log(match);
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    console.log("coordinate ricevute: " + match[1] + ", " + match[2] );
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+  
+  bot.onText(/\/echo (.+)/, function(msg, match) {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    const chatId = msg.chat.id;
+    const resp = "mi hai mandato il testo: " + match[1]; // the captured "whatever"
+    console.log("hai ricevuto il messaggio: " + match[1]);
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+  // Listen for any kind of message. There are different kinds of
+  // messages.
+  /****** commentato per evitare di ricevere messaggi non di testo
+  bot.on('message', function(msg) {
+    const chatId = msg.chat.id;
+    console.log(msg);
+    // send a message to the chat acknowledging receipt of their message
+    
+    bot.sendMessage(chatId, 'Ciao ' + msg.from.first_name + ' ho ricevuto il tuo messaggio!');
+  });
+  *******/
